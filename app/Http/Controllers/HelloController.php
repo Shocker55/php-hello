b8decb487995b8fcb06397380ccbfe2c695e22d3
Shocker55 <bialetti360@gmail.com>
2023-12-11 11:44:41 +0900

    feat: コントローラでテンプレートを表示

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,22 +6,7 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function __invoke() {
-        return <<<EOF
-        <html>
-        <head>
-        <title>Hello/Index</title>
-        <style>
-        body { font-size:16pt; color:#999; }
-        h1 { font-size:100pt; text-align:right; color:#eee;
-             margin:-40px 0px -50px 0px;}
-        </style>
-        </head>
-        <body>
-            <h1>Index</h1>
-            <p>これは、シングルアクションコントローラのアクションです。</p>
-        </body>
-        </html>
-        EOF;
+    public function index() {
+        return view("hello.index");
     }
 }
